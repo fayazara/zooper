fbd5c17cd8cc2eb77b7d2f37f67497d102613755
fayazara <fayazara@gmail.com>
2023-09-19 23:12:21 +0530

    Bookmarks

--- components/App/UsesItem.vue
+++ components/App/UsesItem.vue
@@ -1,9 +1,13 @@
 <template>
   <li>
-    <p class="text-base font-semibold text-gray-700 dark:text-gray-300">
-      {{ item.name }}
-    </p>
-    <p class="text-sm text-gray-500">{{ item.description }}</p>
+    <NuxtLink :to="item.url" class="group" target="_blank" external>
+      <p
+        class="text-base font-semibold text-gray-700 dark:text-gray-300 group-hover:text-primary-600"
+      >
+        {{ item.name }}
+      </p>
+      <p class="text-sm text-gray-500">{{ item.description }}</p>
+    </NuxtLink>
   </li>
 </template>
 
--- pages/bookmarks.vue
+++ pages/bookmarks.vue
@@ -1,19 +1,32 @@
 <template>
   <main class="min-h-screen">
     <AppHeader class="mb-8" title="Bookmarks" :description="description" />
-    <UCommandPalette
-    v-model="selected"
-    nullable
-    :groups="[{ key: 'people', commands: people }]"
-    :fuse="{ resultLimit: 6, fuseOptions: { threshold: 0.1 } }"
-  />
+    <ul class="space-y-2">
+      <li v-for="bookmark in bookmarks" :key="bookmark.id">
+        <a
+          :href="bookmark.url"
+          target="_blank"
+          class="flex items-center gap-3 hover:bg-gray-100 dark:hover:bg-white/10 p-2 rounded-lg -m-2 text-sm min-w-0"
+        >
+          <UAvatar
+            :src="getThumbnail(bookmark.url)"
+            :alt="bookmark.label"
+            :ui="{ rounded: 'rounded-md' }"
+          />
+          <p class="truncate text-gray-700 dark:text-gray-200">
+            {{ bookmark.label }}
+          </p>
+          <span class="flex-1"></span>
+          <span class="text-xs font-medium text-gray-400 dark:text-gray-600">
+            {{ getHost(bookmark.url) }}
+          </span>
+        </a>
+      </li>
+    </ul>
   </main>
 </template>
 
 <script setup>
-const bookmarks = [
-
-]
 const description =
   "Awesome things I've found on the internet. One neat thing about this page is, I just need to send a telegram message to a bot and it adds it here in the list.";
 useSeoMeta({
@@ -21,25 +34,95 @@ useSeoMeta({
   description,
 });
 
-const people = [
-  { id: 1, label: 'Wade Cooper' },
-  { id: 2, label: 'Arlene Mccoy' },
-  { id: 3, label: 'Devon Webb' },
-  { id: 4, label: 'Tom Cook' },
-  { id: 5, label: 'Tanya Fox' },
-  { id: 6, label: 'Hellen Schmidt' },
-  { id: 7, label: 'Caroline Schultz' },
-  { id: 8, label: 'Mason Heaney' },
-  { id: 9, label: 'Claudie Smitham' },
-  { id: 10, label: 'Emil Schaefer' }
-]
-const selected = ref([people[1]])
+const bookmarks = [
+  {
+    id: 1,
+    label: "Adam Wathan - Tailwind CSS Best Practice Patterns",
+    url: "https://www.youtube.com/watch?v=J_7_mnFSLDg",
+  },
+  {
+    id: 2,
+    label: "Dicebear Awesome avatars",
+    url: "https://www.dicebear.com/",
+  },
+  {
+    id: 3,
+    label: "Circuit design stock image",
+    url: "https://images.unsplash.com/photo-1592659762303-90081d34b277?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2873&q=80",
+  },
+  {
+    id: 4,
+    label: "Beautiful Gradient Generator",
+    url: "https://www.joshwcomeau.com/gradient-generator/",
+  },
+  {
+    id: 5,
+    label: "3D device mockups",
+    url: "https://deviceframes.com/",
+  },
+  {
+    id: 6,
+    label: "Box shadow examples",
+    url: "https://getcssscan.com/css-box-shadow-examples",
+  },
+  {
+    id: 7,
+    label: "Octupos Illustration",
+    url: "https://refine.new/",
+  },
+  {
+    id: 8,
+    label: "Metalab agency",
+    url: "https://www.metalab.com/",
+  },
+  {
+    id: 9,
+    label: "Tines - Beautiful landing page",
+    url: "https://www.tines.com/product",
+  },
+  {
+    id: 10,
+    label: "SVG Spinners",
+    url: "https://github.com/n3r4zzurr0/svg-spinners",
+  },
+  {
+    id: 11,
+    label: "ASCII Flow - Text based image drawing",
+    url: "https://asciiflow.com/#/",
+  },
+  {
+    id: 12,
+    label: "REQRES Mock apis for testing",
+    url: "https://reqres.in/",
+  },
+  {
+    id: 13,
+    label: "Haikie - SVG background generator",
+    url: "https://app.haikei.app/",
+  },
+  {
+    id: 14,
+    label: "IP API",
+    url: "https://ipapi.is/",
+  },
+  {
+    id: 15,
+    label: "Rakko Tools",
+    url: "https://en.rakko.tools/",
+  },
+];
 
-// bookmark types
+function getHost(url) {
+  const parsedUrl = new URL(url);
+  let host = parsedUrl.host;
+  if (host.startsWith("www.")) {
+    host = host.substring(4);
+  }
+  return host;
+}
 
-// 1. plain text
-// 2. image
-// 3. youtube video
-// 4. color code
-// 5. link
+function getThumbnail(url) {
+  const host = getHost(url);
+  return `https://logo.clearbit.com/${host}`;
+}
 </script>
