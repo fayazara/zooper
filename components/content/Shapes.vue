40fd377dfa3495105c4f2662cb6aa3f660375bdb
Fayaz Ahmed <fayazara@gmail.com>
2023-11-14 12:36:12 +0530

    New experiment

---
+++ components/content/Shapes.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex w-full items-center gap-4">
+    <div class="flex flex-col items-center gap-3">
+      <div
+        class="w-0 h-0 border-x-[30px] border-x-transparent border-b-[50px] border-b-gray-800"
+      />
+      <p class="text-xs font-semibold text-gray-500">Triangle</p>
+    </div>
+    <div class="flex flex-col items-center gap-3">
+      <div
+        class="relative w-0 h-0 my-[17.5px] mx-0 border-x-[35px] border-x-transparent border-b-[24.5px] border-b-red-500 rotate-[35deg] before:content-[''] before:absolute before:w-0 before:h-0 before:border-b-[28px] before:border-b-red-500 before:border-x-[10.5px] before:border-x-transparent before:top-[-15.75px] before:left-[-22.75px] before:rotate-[-35deg] after:content-[''] after:absolute after:w-0 after:h-0 after:border-x-[35px] after:border-b-[24.5px] after:border-b-red-500 after:border-x-transparent after:top-[1.05px] after:left-[-36.75px] after:rotate-[-70deg]"
+      ></div>
+      <p class="text-xs font-semibold text-gray-500">Star</p>
+    </div>
+    <div class="flex flex-col items-center gap-3">
+      <div
+        class="w-0 h-0 border-x-[20px] border-x-transparent border-y-[25px] border-y-indigo-500"
+      ></div>
+      <p class="text-xs font-semibold text-gray-500">Hourglass</p>
+    </div>
+    <div class="flex flex-col items-center gap-3">
+      <div class="w-20 h-12 bg-teal-500 -skew-x-[16deg]"></div>
+      <p class="text-xs font-semibold text-gray-500">Parallelogram</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
